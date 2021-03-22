85fd717e9ef245f9104d9e977e74d1e4cfb7b8d9
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-03-22 17:39:22 +0900

    Create -> Project

---
+++ hewi/hewi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hewi
+//
+//  Created by 조민서 on 2021/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
