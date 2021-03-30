d863cdc11677409cef6c9782c7c083a16d0774d1
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-03-30 16:10:02 +0900

    Create -> 메인화면 CollectionView 제작

--- hewi_swiftUI/hewi/ContentView.swift
+++ hewi_swiftUI/hewi/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let data = Array(1...100).map { "Item \($0)" }
+    
+    private var columns : [GridItem] = [
+        GridItem(.fixed(100), spacing: 16),
+        GridItem(.fixed(100), spacing: 16)
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("HeWi")
+                .font(.title)
+                .foregroundColor(.black)
+                .padding()
+            
+            Spacer()
+            
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20){
+                    ForEach(data, id: \.self){ item in
+                        Text(item)
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+        }
     }
 }
 
