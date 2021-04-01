ef28a646bfb9f741a073dde27a6bda2aaaee5ba5
JoMinSeo <49663640+JoMinSeo@users.noreply.github.com>
2021-04-01 12:32:16 +0900

    Fix -> list => lazyVGrid

---
+++ hewi_swiftUI/hewi/Views/AlbumView.swift
@@ -0,0 +1,19 @@
+//
+//  AlbumView.swift
+//  hewi
+//
+//  Created by 조민서 on 2021/04/01.
+//
+
+import SwiftUI
+
+struct AlbumView : View {
+    
+    var body: some View {
+        VStack() {
+            Text("우이")
+        }
+        .navigationBarTitle("Hello")
+    }
+    
+}
